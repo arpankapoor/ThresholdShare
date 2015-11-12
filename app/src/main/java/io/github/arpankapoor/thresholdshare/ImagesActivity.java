2a74c2d96406b78942a13a54c4321c97f50f1ee5
Arpan Kapoor <k.arpan26@gmail.com>
2015-11-13 04:08:41 +0530

    Add ImagesActivity

--- app/src/main/java/io/github/arpankapoor/thresholdshare/ImagesActivity.java
+++ app/src/main/java/io/github/arpankapoor/thresholdshare/ImagesActivity.java
@@ -2,26 +2,25 @@ package io.github.arpankapoor.thresholdshare;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
@@ -32,6 +31,7 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -47,53 +47,113 @@ import io.github.arpankapoor.db.UserContract;
 import io.github.arpankapoor.message.Message;
 import io.github.arpankapoor.user.User;
 
-public class MainActivity extends AppCompatActivity {
+public class ImagesActivity extends AppCompatActivity {
 
-    private int userId;
     private DbHelper dbHelper;
+    private int userId = -1;
+    private int senderId = -1;
 
     private void setList() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        // Get users who have sent this user a message
         String rawSql =
-                "SELECT DISTINCT " +
-                        UserContract.UserEntry.COLUMN_NAME_NAME + ", " +
-                        UserContract.UserEntry.TABLE_NAME + "." + UserContract.UserEntry._ID +
-                        " FROM " + UserContract.UserEntry.TABLE_NAME +
-                        " INNER JOIN " +
-                        MessageContract.MessageEntry.TABLE_NAME +
-                        " ON " +
-                        UserContract.UserEntry.TABLE_NAME + "." + UserContract.UserEntry._ID +
-                        " = " +
-                        MessageContract.MessageEntry.TABLE_NAME + "." +
-                        MessageContract.MessageEntry.COLUMN_NAME_SENDER_ID;
+                "SELECT * " +
+                        " FROM " + MessageContract.MessageEntry.TABLE_NAME +
+                        " WHERE " +
+                        MessageContract.MessageEntry.COLUMN_NAME_SENDER_ID + " = " +
+                        senderId;
+
         Log.v("HI", rawSql);
 
         Cursor cursor = db.rawQuery(rawSql, null);
 
-        String[] columns = new String[]{UserContract.UserEntry.COLUMN_NAME_NAME};
+        String[] columns = new String[]{MessageContract.MessageEntry.COLUMN_NAME_FILENAME};
         SimpleCursorAdapter mAdapter =
                 new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, columns,
                         new int[]{android.R.id.text1}, CursorAdapter.FLAG_AUTO_REQUERY);
 
-        ListView listView = (ListView) findViewById(R.id.messages_list_view);
+        ListView listView = (ListView) findViewById(R.id.images_list_view);
         listView.setAdapter(mAdapter);
     }
 
+    private void waitAlert() {
+        new AlertDialog.Builder(this)
+                .setMessage("Please wait for others to provide their decryption key.")
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                .show();
+    }
+
+    private void sendKeyAlert(final int messageId) {
+        new AlertDialog.Builder(this)
+                .setMessage("Send your key for decryption?")
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                new SendKeyTask().execute(messageId);
+                                dialog.dismiss();
+                            }
+                        })
+                .show();
+    }
+
+    private void updateMessageStatus(int messageId, int status) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(MessageContract.MessageEntry.COLUMN_NAME_STATUS, status);
+
+        db.update(MessageContract.MessageEntry.TABLE_NAME, values,
+                MessageContract.MessageEntry._ID + " = " + messageId, null);
+    }
+
     private void setListViewListener() {
-        final ListView listView = (ListView) findViewById(R.id.messages_list_view);
+        final ListView listView = (ListView) findViewById(R.id.images_list_view);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Cursor cursor = (Cursor) listView.getItemAtPosition(position);
-                int senderIdColumnIndex = cursor.getColumnIndex(UserContract.UserEntry._ID);
-                int senderId = cursor.getInt(senderIdColumnIndex);
+                int statusColumnIndex = cursor.getColumnIndex(
+                        MessageContract.MessageEntry.COLUMN_NAME_STATUS);
+                int status = cursor.getInt(statusColumnIndex);
 
-                Intent intent = new Intent(getApplicationContext(), ImagesActivity.class);
-                intent.putExtra("sender_id", senderId);
-                intent.putExtra("user_id", userId);
-                startActivity(intent);
+                int messageIdColumnIndex = cursor.getColumnIndex(
+                        MessageContract.MessageEntry._ID);
+                int messageId = cursor.getInt(messageIdColumnIndex);
+
+                int filenameColumnIndex = cursor.getColumnIndex(
+                        MessageContract.MessageEntry.COLUMN_NAME_FILENAME);
+                String filename = cursor.getString(filenameColumnIndex);
+
+                if (status == MessageContract.MessageEntry.STATUS_KEY_RCVD) {
+                    // Give option to send image
+                    sendKeyAlert(messageId);
+                } else if (status == MessageContract.MessageEntry.STATUS_KEY_SENT) {
+                    // Please wait
+                    waitAlert();
+                } else {
+                    // Show image
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    File file = new File(getFilesDir(), filename);
+                    Uri uri = FileProvider.getUriForFile(getApplicationContext(),
+                            "io.github.arpankapoor", file);
+                    intent.setDataAndType(uri, "image/*");
+                    intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+                }
             }
         });
     }
@@ -101,56 +161,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_images);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         dbHelper = new DbHelper(this);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(
-                getString(R.string.preference_file), MODE_PRIVATE);
-
-        userId = sharedPreferences.getInt("id", -1);
-
-        if (userId == -1) {
-            Intent intent = new Intent(this, RegisterationActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendMessageIntent = new Intent(view.getContext(), SendMessageActivity.class);
-                startActivity(sendMessageIntent);
-            }
-        });
+        Intent intent = getIntent();
+        senderId = intent.getIntExtra("sender_id", 0);
+        userId = intent.getIntExtra("user_id", -1);
 
         setListViewListener();
         callAsynchronousTask();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void callAsynchronousTask() {
@@ -172,6 +195,71 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         timer.schedule(doAsynchronousTask, 0, 10000);
+    }
+
+    private class SendKeyTask extends AsyncTask <Integer, Void, Void> {
+        private String getKey(int messageId) {
+            SQLiteDatabase db = dbHelper.getReadableDatabase();
+            Cursor cursor = db.query(MessageContract.MessageEntry.TABLE_NAME,
+                    new String[]{MessageContract.MessageEntry.COLUMN_NAME_KEY},
+                    MessageContract.MessageEntry._ID + " = " + messageId,
+                    null, null, null, null);
+
+            cursor.moveToFirst();
+            int keyColumnIndex = cursor.getColumnIndex(
+                    MessageContract.MessageEntry.COLUMN_NAME_KEY);
+
+            return cursor.getString(keyColumnIndex);
+        }
+
+        @Override
+        protected Void doInBackground(Integer... params) {
+            int messageId = params[0];
+            String key = getKey(messageId);
+            if (key == null || messageId == -1 || userId == -1) {
+                return null;
+            }
+
+            Uri uri = Uri.parse(getString(R.string.server_base_url))
+                    .buildUpon()
+                    .appendPath(getString(R.string.send_key_api))
+                    .build();
+
+            JSONObject jsonObject = new JSONObject();
+            HttpURLConnection connection = null;
+            DataOutputStream dos;
+            try {
+                jsonObject.put("id", userId);
+                jsonObject.put("message_id", messageId);
+                jsonObject.put("key", key);
+                Log.v("HI", userId + "");
+
+                String jsonStr = jsonObject.toString();
+                Log.v("HI", jsonStr);
+                URL url = new URL(uri.toString());
+
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json;");
+                connection.connect();
+
+                dos = new DataOutputStream(connection.getOutputStream());
+                dos.writeBytes(jsonStr);
+                dos.flush();
+                dos.close();
+                connection.getInputStream();
+            } catch (Exception ex) {
+                //
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            updateMessageStatus(messageId, MessageContract.MessageEntry.STATUS_KEY_SENT);
+            return null;
+        }
     }
 
     private class GetMessagesTask extends AsyncTask<Void, Void, List<Message>> {
